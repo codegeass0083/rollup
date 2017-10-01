7c7e91e1818cede7722a0c3d34d11117867543ac
Chun Tang <chun@madhattertech.ca>
2017-10-01 19:57:17 -0400

    updated rollup

--- rollup.go
+++ rollup.go
@@ -1,126 +1,105 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type key struct {
-	year  string
-	month string
-}
-
 func main() {
-	// args: inputPath, y, m, d
-	commandLineArgs := os.Args[1:]
-	if len(commandLineArgs) < 1 || len(commandLineArgs) > 4 {
-		fmt.Println("Wrong number of arguments. Usage: ./rollup [input_file_path y m d]")
-		os.Exit(1)
-	}
-	// read data from input file
-	tsvFile, err := os.Open(commandLineArgs[0])
-	checkErr(err)
-	defer tsvFile.Close()
-
-	// validate column arguments
-	columnStr := strings.Join(commandLineArgs[1:], "")
-	if columnStr != "y" && columnStr != "ym" && columnStr != "ymd" && columnStr != "" {
-		fmt.Println("Invalid input for columns, only accept [y, m, d], [y, m], [y], []")
-		os.Exit(1)
-	}
-
+	var err error
+	outputColumns := os.Args[1:]
+	outputFirstLine := strings.Join(outputColumns, "\t") + "\tvalue"
+	printOutput(outputFirstLine)
 	// data processing
-	scanner := bufio.NewScanner(tsvFile)
-	if !scanner.Scan() {
-		fmt.Println("Input is empty.")
-		return
-	}
-	firstLine := scanner.Text()
-	// check first line
-	if firstLine != "y\tm\td\tvalue" {
-		fmt.Println("Input file has wrong format.")
-		return
-	}
-	fmt.Println(firstLine)
-	ymMap := make(map[key]int64)
-	yMap := make(map[string]int64)
-	for scanner.Scan() {
-		record := scanner.Text()
-		recordSlice := strings.Split(record, "\t")
-		// validate data in each row
-		y, err := strconv.ParseInt(recordSlice[0], 10, 64)
-		if err != nil || y <= 0 {
-			continue
-		}
-		m, err := strconv.ParseInt(recordSlice[1], 10, 64)
-		if err != nil || m < 1 || m > 12 {
-			continue
-		}
-		d, err := strconv.ParseInt(recordSlice[2], 10, 64)
-		if err != nil || !isValidDay(m, d) {
-			continue
-		}
-		if len(recordSlice) < 4 {
-			//fmt.Printf("[DEBUG] Invalid row %s, skipping ..\n", record)
-			continue
-		}
-		if columnStr == "ymd" || columnStr == "" {
-			// print [y m d]
-			fmt.Println(record)
-			checkErr(err)
-		}
-		recordKey := key{year: recordSlice[0], month: recordSlice[1]}
-		val, err := strconv.ParseInt(recordSlice[3], 10, 64)
-		//fmt.Printf("[DEBUG] %v\n", val)
-		checkErr(err)
-		if _, ok := ymMap[recordKey]; ok {
-			ymMap[recordKey] += val
-		} else {
-			ymMap[recordKey] = val
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
-	for k, v := range ymMap {
-		//fmt.Printf("[DEBUG] ymMap k %s %s v %d\n", k.year, k.month, v)
-		if columnStr != "y" {
-			// print [y m]
-			fmt.Println(k.year + "\t" + k.month + "\t\t" + strconv.FormatInt(v, 10))
-			checkErr(err)
-		}
-		if _, ok := yMap[k.year]; ok {
-			yMap[k.year] += v
-		} else {
-			yMap[k.year] = v
-		}
-	}
-	var sum int64
-	for k, v := range yMap {
-		// print [y]
-		fmt.Println(k + "\t\t\t" + strconv.FormatInt(v, 10))
-		checkErr(err)
-		sum += v
-	}
-	// print []
-	fmt.Println("\t\t\t" + strconv.FormatInt(sum, 10))
+	input, err := ioutil.ReadAll(os.Stdin)
 	checkErr(err)
+	lines := strings.Split(string(input), "\n")
+	inputColumnString := lines[0]
+	records := lines[1:]
+	inputColumns := strings.Split(inputColumnString, "\t")
+	// convert records slice to map and do initial aggregation
+	recordMap := make(map[string]int64)
+	for _, record := range records {
+		key, value := generateKV(record, outputColumns, inputColumns)
+		if _, ok := recordMap[key]; ok {
+			recordMap[key] += value
+		} else {
+			recordMap[key] = value
+		}
+	}
+	printMap(recordMap, 1)
+	// aggregating over all the prefix of output columns and print to stdout
+	for i := 0; i < len(outputColumns)-1; i++ {
+		recordMap = rollupAndPrint(recordMap, i+2)
+	}
+	rollupAndPrint(recordMap, len(outputColumns))
 }
 
-func isValidDay(month, day int64) bool {
-	if day < 1 {
-		return false
+func rollupAndPrint(recordMap map[string]int64, tabCount int) map[string]int64 {
+	ret := make(map[string]int64)
+	for k, v := range recordMap {
+		key := removeLastColumnFromKey(k)
+		if _, ok := ret[key]; ok {
+			ret[key] += v
+		} else {
+			ret[key] = v
+		}
 	}
-	if month == 2 {
-		return day <= 29 // TODO: check leap year
+	printMap(ret, tabCount)
+	return ret
+}
+
+func removeLastColumnFromKey(key string) string {
+	keySlice := strings.Split(key, "\t")
+	return strings.Join(keySlice[:len(keySlice)-1], "\t")
+}
+
+func generateKV(record string, output, input []string) (string, int64) {
+	keyIndex := generateKeyIndex(output, input)
+	recordSlice := strings.Split(record, "\t")
+	recordLen := len(recordSlice)
+	value, err := strconv.ParseInt(recordSlice[recordLen-1], 10, 64)
+	checkErr(err)
+	var buffer bytes.Buffer
+	for _, i := range keyIndex {
+		buffer.WriteString(recordSlice[i] + "\t")
 	}
-	if month == 4 || month == 6 || month == 9 || month == 11 {
-		return day <= 30
+	key := buffer.String()
+	return key[:len(key)-1], value
+}
+
+func generateKeyIndex(output, input []string) []int {
+	ret := make([]int, len(output))
+	for index, outputStr := range output {
+		for i, inputStr := range input {
+			if inputStr == outputStr {
+				ret[index] = i
+			}
+		}
 	}
-	return day <= 31
+	return ret
+}
+
+func printMap(recordMap map[string]int64, tabCount int) {
+	for k, v := range recordMap {
+		printOutput(k + getTabs(tabCount) + strconv.FormatInt(v, 10))
+	}
+}
+
+func getTabs(count int) string {
+	var ret bytes.Buffer
+	for i := 0; i < count; i++ {
+		ret.WriteByte('\t')
+	}
+	return ret.String()
+}
+
+func printOutput(line string) {
+	fmt.Println(line)
 }
 
 func checkErr(err error) {
